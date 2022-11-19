7d215d21b2ca387746fb7317d225d9cf9257a68f
Matthew <mrcockburn@rogers.com>
2022-11-19 11:20:15 -0400

    more doxygen comments

--- Alex_RTOS_Sailboat/RTOS_framework_v1/src/sail_buffer.h
+++ Alex_RTOS_Sailboat/RTOS_framework_v1/src/sail_buffer.h
@@ -1,8 +1,10 @@
-/* sail_buffer.h
+/** sail_buffer.h
+ * @file
+ * @brief 
  * Header file for the buffer module used to manage the producer-consumer problem for the
  * interrupt driven UART driver.
  * Created on July 11, 2016.
- * Created by Thomas Gwynne-Timothy.
+ * @author Thomas Gwynne-Timothy.
  */
 
 
@@ -14,13 +16,14 @@
 #include <stdbool.h>
 
 
-/* BUFF_Module
- * Structure to manage the configuration and state of a buffer.
- * buffer		- pointer to the start of the buffer
- * read_ptr		- pointer last unread byte
- * write_ptr	- pointer to the first empty byte
- * capacity		- maximum capacity of the buffer
- * length		- current number of elements in the buffer
+/** BUFF_Module
+ * @struct BUFF_Module
+ * @brief Structure to manage the configuration and state of a buffer.
+ * @var buffer    - pointer to the start of the buffer
+ * @var read_ptr  - pointer last unread byte
+ * @var write_ptr - pointer to the first empty byte
+ * @var capacity  - maximum capacity of the buffer
+ * @var length	  - current number of elements in the buffer
  */
 typedef struct BUFF_Module {
 	volatile uint8_t *buffer;
@@ -30,16 +33,105 @@ typedef struct BUFF_Module {
 	volatile uint16_t length;
 } BUFF_Module;
 
+
+/**
+ * @brief
+ * Initialize new empty ring buffer 
+ * @param mod - Pointer to the ring buffer structure 
+ * @param buffer - Pointer to the buffer data
+ * @param size - size of the ring buffer 
+ * @return STATUS_ERR_BAD_ADDRESS - When a null pointer is passed 
+ * @return STATUS_OK - Buffer was successfully implemented 
+ */
 enum status_code BUFF_Init(BUFF_Module *mod, volatile uint8_t *buffer, uint16_t size);
 
+
+/**
+ * @brief
+ * Reads a byte of data from the buffer
+ * @param mod - Pointer to the buffer module
+ * @param data - Pointer to fill byte read 
+ * @return STATUS_VALID_DATA - When a byte has been read successfully 
+ * @return STATUS_ERR_BAD_ADDRESS - If a null pointer is passed 
+ * @return STATUS_NO_CHANGE - If no new data is in the buffer
+ */
 enum status_code BUFF_ReadByte(BUFF_Module *mod, uint8_t *data);
+
+
+/**
+ * @brief
+ * Writes a byte to a buffer
+ * @param mod - Pointer to the buffer 
+ * @param data - byte to be written
+ * @return STATUS_VALID_DATA - When a byte has been read successfully
+ * @return STATUS_ERR_BAD_ADDRESS - If a null pointer is passed
+ */
 enum status_code BUFF_WriteByte(BUFF_Module *mod, uint8_t data);
 
+
+/**
+ * @brief
+ * Read a string of bytes from a buffer. 
+ * Will return early if a terminating character is detected in buffer
+ * @param mod - Pointer to ring buffer
+ * @param data - Pointer to string to be filled 
+ * @param length - Size of string to be filled 
+ * @return STATUS_VALID_DATA - When a byte has been read successfully
+ * @return STATUS_ERR_BAD_ADDRESS - If a null pointer is passed
+ * @return STATUS_NO_CHANGE - If no new data is in the buffer
+ * @return STATUS_ERR_NO_MEMORY - If the string found is to big
+ */
 enum status_code BUFF_ReadString(BUFF_Module *mod, uint8_t *data, uint16_t length);
+
+
+/**
+ * @brief
+ * Read bytes of data from a buffer module 
+ * @param mod - Pointer to ring buffer
+ * @param data - Pointer to buffer to be filled
+ * @param length - Size of buffer to be filled
+ * @return STATUS_VALID_DATA - When a byte has been read successfully
+ * @return STATUS_ERR_BAD_ADDRESS - If a null pointer is passed
+ * @return STATUS_NO_CHANGE - If no new data is in the buffer
+ * @return STATUS_ERR_NO_MEMORY - If the string found is to big
+ */
 enum status_code BUFF_ReadBuffer(BUFF_Module *mod, uint8_t *data, uint16_t length);
+
+
+/**
+ * @brief
+ * Write a string of bytes to a buffer module  
+ * @param mod - Pointer to the buffer module
+ * @param data - Pointer to string of bytes being written
+ * @param length - Number of bytes to be written 
+ * @return STATUS_OK - Data has been transmitted
+ * @return STATUS_ERR_BAD_ADDRESS - A null pointer was passed
+ * @return STATUS_ERR_NO_MEMORY - Buffer does not have sufficient space to store message
+ */
 enum status_code BUFF_WriteBuffer(BUFF_Module *mod, uint8_t *data, uint16_t length);
+
+
+/**
+ * @brief
+ * Write a string of bytes to a buffer module, without using the mutex.
+ * @note This API is not thread safe 
+ * @param data - Pointer to string of bytes being written
+ * @param length - Number of bytes to be written
+ * @return STATUS_OK - Data has been transmitted
+ * @return STATUS_ERR_BAD_ADDRESS - A null pointer was passed
+ * @return STATUS_ERR_NO_MEMORY - Buffer does not have sufficient space to store message
+ */
 enum status_code BUFF_WriteBufferUnprotected(BUFF_Module *mod, uint8_t *data, uint16_t length);
 
+
+/**
+ * @brief
+ * Returns the length of a buffer module
+ * @param mod - Pointer to buffer module
+ * @param length - Pointer to length value to be assigned
+ * @return STATUS_ERR_BAD_ADDRESS - If a null pointer is passed
+ * @return STATUS_OK - If length of buffer is returned
+ */
 enum status_code BUFF_GetLength(BUFF_Module *mod, uint16_t *length);
 
 #endif /* SAIL_BUFFER_H_ */
--- Alex_RTOS_Sailboat/RTOS_framework_v1/src/sail_uart.h
+++ Alex_RTOS_Sailboat/RTOS_framework_v1/src/sail_uart.h
@@ -1,9 +1,9 @@
 /** sail_uart.h
  * @file
  * @brief
- * Header file for the UART driver module for the autonomous sailboat project.
+ * UART driver API's for the autonomous sailboat project.
  * Created on June 13, 2016.
- * Created by Thomas Gwynne-Timothy.
+ * @author Thomas Gwynne-Timothy.
  */
 
 #ifndef SAIL_UART_H
@@ -22,46 +22,74 @@ typedef enum UART_ChannelIDs {
 } UART_ChannelID;
 
 
-//UART_Init
-// Initialize a specific UART port.
-// Inputs:
-//   id - ID of the specified UART port
-// Status:
-//   STATUS_OK - UART was initialized successfully
-//   STATUS_ERR_INVALID_ARG - The provided ID is invalid
-//   STATUS_ERR_DENIED - A driver error occurred
-//
+
+/**
+ * @brief Initialize a specific UART port.
+ * @param id - ID of the specified UART port
+ * @return STATUS_OK - UART was initialized successfully 
+ * @return STATUS_ERR_DENIED - A driver error occurred
+ * @return STATUS_ERR_INVALID_ARG - The provided ID is invalid  
+ */
 enum status_code UART_Init(UART_ChannelID id);
 
 
-// UART_Enable
-// Starts the receive job for the specified buffer.
-//
+/**
+ * @brief Starts the receive job for the specified buffer.
+ * @param id - ID of the specified UART port
+ * @return STATUS_OK - UART was enabled successfully
+ * @return STATUS_ERR_NOT_INITIALIZED - If the UART Module is not initialized 
+ * @return STATUS_NO_CHANGE - If the UART Module is already Enabled 
+ */
 enum status_code UART_Enable(UART_ChannelID id);
 
 
-// UART_Disable
-// Stops the receiver from acquiring anymore data.
-//
+/**
+ * @brief Stops the receiver from acquiring anymore data.
+ * @param id - ID of the specified UART port
+ * @return STATUS_OK - UART was disabled successfully
+ * @return STATUS_ERR_NOT_INITIALIZED - If the UART Module is not initialized 
+ * @return STATUS_NO_CHANGE - If the UART Module is already disabled 
+ */
 enum status_code UART_Disable(UART_ChannelID id);
 
 
-// UART_TxString
-// Sends a null-terminated string with the specified UART.
-//
+/**
+ * @brief Sends a null-terminated string with the specified UART.
+ * @param id - ID of the specified UART port
+ * @param data - Pointer to string of data to be transmitted
+ * @return STATUS_OK - Data was transmitted successfully 
+ * @return STATUS_ERR_BAD_ADDRESS - If a null pointer is detected 
+ * @return STATUS_ERR_INVALID_ARG - If the ID passed is invalid   
+ */
 enum status_code UART_TxString(UART_ChannelID id, uint8_t *data);
 
-// UART_TxString_Unprotected
-// The same as UART_TxString except it does not call the mutex protecting the
-// circular buffer, so the function can be used before the freeRTOS scheduler is started
-//
+
+/**
+ * @brief The same as UART_TxString except it does not call the mutex protecting the circular buffer,
+ * so the function can be used before the freeRTOS scheduler is started
+ *
+ * @param id - ID of the specified UART port
+ * @param data - Pointer to string of data to be transmitted
+ * @return STATUS_OK - Data was transmitted successfully
+ * @return STATUS_ERR_BAD_ADDRESS - If a null pointer is detected
+ * @return STATUS_ERR_INVALID_ARG - If the ID passed is invalid
+ */
+
 enum status_code UART_TxString_Unprotected(UART_ChannelID id, uint8_t *data);
 
 
-// UART_RxString
-// Parses the FIFO buffer for a string (delimited with \r, \n, or \0)
-// and copies it to the provided memory.
-//
+/**
+ * @brief Parses the FIFO buffer for a string (delimited with \r, \n, or \0)
+ * and copies it to the provided memory.
+ *
+ * @param id - ID of the specified UART port
+ * @param data - Pointer to string of data to be transmitted
+ * @param length - Length of the amount of bytes to be read from buffer 
+ * @return STATUS_VALID_DATA - If buffer has been filled
+ * @return STATUS_NO_CHANGE - No new data is in the UART buffer	
+ * @return STATUS_ERR_NO_MEMORY - Not enough memory allocated for the buffer
+ * @return STATUS_ERR_DENIED - Error with UART driver layer has occurred 
+ */
 enum status_code UART_RxString(UART_ChannelID id, uint8_t *data, uint16_t length);
 
 #endif // SAIL_UART_H
