af9bedec5bc611808ac891762c6c5b7dd5c82d84
HargunSohi <sohihargun05@gmail.com>
2024-06-29 13:20:04 -0300

    This commit is empty, it is just to initialize.
    First Commit

---
+++ Nautono/RTOS_framework_v1/src/config/sail_imu2.c
@@ -0,0 +1,6 @@
+/*
+ * sail_imu2.c
+ *
+ * Created: 6/29/2024 1:16:21 PM
+ *  Author: hargun
+ */ 
---
+++ Nautono/RTOS_framework_v1/src/sail_imu2.h
@@ -0,0 +1,6 @@
+/*
+ * sail_imu2.h
+ *
+ * Created: 6/29/2024 1:17:35 PM
+ *  Author: hargun
+ */ 
